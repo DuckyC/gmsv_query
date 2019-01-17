6e54fa7ae5dddf30da6438786b21814fc239a144
Sam Nyashev <12237569+Heyter@users.noreply.github.com>
2019-01-17 21:39:02 +0300

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,19 +1,25 @@
---if true then return end
-require("query")
+if (!istable(query)) then
+	require("query")
+end
+
 query.EnableInfoDetour(true)
 
 print("Detour enabled")
 hook.Add("A2S_INFO", "reply", function(ip, port, info)
     print("A2S_INFO from", ip, port)
+	
     info.players = 100
+    info.map = 'newbie'
+	
     return info
 end)
 
 hook.Add("A2S_PLAYER", "reply", function(ip, port, info)
     print("A2S_PLAYER from", ip, port)
+	
     return {
-        {name = "DUCKS", score = 10, time = 10},
-        {name = "DUCKS", score = 10, time = 10},
-        {name = "DUCKS", score = 10, time = 10},
+        {name = "DUCKS1", score = 3, time = 1},
+		{name = "DUCKS2", score = 2, time = 2},
+		{name = "DUCKS3", score = 1, time = 3},
     }
 end)
